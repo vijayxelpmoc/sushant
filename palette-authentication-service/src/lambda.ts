0e6995e88ab5a9ea135aa4eb2d562db59984c8c0
Shaiq Kar <shaiqkar@gmail.com>
2022-04-26 15:25:54 +0530

    new lambda proxy

--- palette-authentication-service/src/lambda.ts
+++ palette-authentication-service/src/lambda.ts
@@ -1,29 +1,6 @@
 // tslint:disable-next-line:no-var-requires
 require('module-alias/register');
 
-// import serverlessExpress from '@vendia/serverless-express';
-// import { Callback, Context, Handler } from 'aws-lambda';
-
-// import { preBuildApp } from './main';
-
-// let server: Handler;
-
-// async function bootstrap() {
-//   const app = await preBuildApp();
-//   await app.init();
-//   const expressApp = app.getHttpAdapter().getInstance();
-//   return serverlessExpress({ app: expressApp });
-// }
-
-// export const handler: Handler = async (
-//   event: any,
-//   context: Context,
-//   callback: Callback,
-// ) => {
-//   server = server ?? (await bootstrap());
-//   return server(event, context, callback);
-// };
-
 import { NestFactory, Reflector } from '@nestjs/core';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
