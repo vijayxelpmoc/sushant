5e9053387c5e6365b22b68809a190f691314cab6
Kshitij Kumar <kshitijkumar176@gmail.com>
2022-05-12 16:57:52 +0530

    utility module

--- palette-utility-service/src/modules/utility/types/index.ts
+++ palette-utility-service/src/modules/utility/types/index.ts
@@ -1,6 +1,6 @@
 export interface SFGuide {
   Name: string;
-  Guide_Description__c: string;
-  Event_String__c: string;
-  Role__c: string;
+  Guide_Description: string;
+  Event_String: string;
+  Role: string;
 }
