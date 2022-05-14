a300f5bd9d2d64d87c724028ce99d10ddb48c50e
Kshitij Kumar <kshitijkumar176@gmail.com>
2022-05-14 16:01:39 +0530

    Report issue fixed

--- palette-utility-service/src/modules/utility/utility.module.ts
+++ palette-utility-service/src/modules/utility/utility.module.ts
@@ -2,10 +2,49 @@ import { Module } from '@nestjs/common';
 
 import { UtilityService } from './utility.service';
 import { UtilityController } from './utility.controller';
+import { HttpModule } from '@nestjs/axios';
+import { MailerModule, MailerService } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { SfModule } from '@gowebknot/palette-salesforce-service';
 
 @Module({
+  imports: [
+    SfModule,
+    HttpModule,
+    MailerModule.forRoot({
+      transport: {
+        // Through Etheral
+        // host: 'smtp.ethereal.email',
+        // port: 587,
+        // logger: true,
+        // debug: true,
+        // secureConnection: false,
+        // auth: {
+        //   user: 'kendall91@ethereal.email',
+        //   pass: 'wq4wsGbt2dhFgjrpHk',
+        // },
+
+        // Through Gmail services (remember to disbale secure apps in settings)
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        service: 'gmail',
+        auth: {
+          user: 'noreplypaletteapp@gmail.com',
+          pass: 'paletteemailservice',
+        },
+      },
+      template: {
+        dir: process.cwd() + '/src/common/mailer/template/',
+        adapter: new HandlebarsAdapter(), // or new PugAdapter()
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
   controllers: [UtilityController],
   providers: [UtilityService],
-  exports: [UtilityService]
+  exports: [UtilityService],
 })
 export class UtilityModule {}
--- palette-utility-service/src/modules/utility/utility.service.ts
+++ palette-utility-service/src/modules/utility/utility.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Injectable,
   InternalServerErrorException,
   NotFoundException,
@@ -13,13 +14,24 @@ import { Errors, Responses } from '@src/constants';
 import { ContactInfoDto, FeedbackInfoDto, ReportIssueDto } from './dto';
 import { SFGuide } from './types';
 import { SfService } from '@gowebknot/palette-salesforce-service';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class UtilityService {
   _notifier: Notifier;
-  constructor(private sfService: SfService) {
+  constructor(
+    private sfService: SfService,
+    private mailerService:MailerService,
+    private httpService: HttpService,
+  ) {
     this._notifier = new Notifier();
   }
+
+  accessToken = `fb3bb87ed25d47fcbba3674a1f75e5194964c66322826ef30c405095134bea57`;
+  NewPortalUserUrl = `https://palette-bigthought.ideas.aha.io/api/v1/idea_portals/7065254068546852736/portal_users/?access_token=${this.accessToken}`;
+  NewIdeaUrl = `https://palette-bigthought.ideas.aha.io/api/v1/products/6981847117522761702/ideas/?access_token=${this.accessToken}`;
 
   async contactUs(contactInfoDto: ContactInfoDto, instituteId: string) {
     const { email, message, name } = contactInfoDto;
@@ -61,9 +73,20 @@ export class UtilityService {
   }
 
   async addReportIssue(reportIssueDto: ReportIssueDto, instituteId: string) {
-    const { email, message, name, screenshots } = reportIssueDto;
+    const { email, message, name, type, category, needed_by, screenshots } =
+      reportIssueDto;
+    // type is a string of categories separrated with commas
+    let userId = null;
 
-    const response = await this.sfService.models.reportIssues.create(
+    const nameArray = name.split(' ');
+    const first_name = nameArray[0];
+    const last_name = nameArray[1];
+
+    // joining all the urls and making a single string
+    // const screenshotsValue = screenshots.join(',\n');
+
+    // saving on salesforce service
+    await this.sfService.models.reportIssues.create(
       {
         User_Name: name,
         Email: email,
@@ -74,40 +97,123 @@ export class UtilityService {
       },
       instituteId,
     );
-
-    console.log(response);
-    
-
-    if (response.success) {
-      // Send a notification to the admin
-      // All the recipients of report issue are stored in env:UTILITY_NOTIFICATION_RECIPIENTS
-
-      const body = `
-      A new issue was report on Palette app at ${new Date()}. </br> 
-      Name: ${name} </br>
-      Email: <a href="mailto:${email}">${email} </br>
-      Message: ${message} </br>
-      
-      Reference Screenshots : </br>
-      ${screenshots.map(
-        (screenshot, idx) => `<a href="${screenshot}">Image ${idx + 1}</a>`,
-      )}`;
-
-      const config: NotificationTypeEmail = {
-        to: process.env.UTILITY_NOTIFICATION_RECIPIENTS.split(','),
-        subject: '[NEW] Issue Reported from Palette',
-        body,
-      };
-
-      this._notifier.send(NotificationType.EMAIL, config);
-
-      return {
-        statusCode: 200,
-        message: Responses.REPORT_ISSUE_SUCCESS,
-      };
+    const screenShotList = [];
+    for (let i = 0; i < 3; i++) {
+      if (screenshots[i]) {
+        screenShotList.push(screenshots[i]);
+      }
     }
+    // send this report as email
+    this.sendReportEmail(
+      [
+        'sumanth.s@gowebknot.com',
+        'palette_app@paletteedu.net',
+        'ideas-PEW-pallette@iad-prod2.mailer.aha.io',
+        // 'kshitijkumar176@gmail.com',
+      ],
+      name,
+      email,
+      message,
+      screenShotList,
+    );
 
-    throw new InternalServerErrorException(Errors.REPORT_ISSUE_FAILED);
+    // Function to create the idea after user creation
+    const post_idea = async () => {
+      const get_Users = await this.getAllPortalUsers();
+
+      // checking and creating ideea.
+      get_Users.subscribe(async (user) => {
+        // console.log('get_user', user);
+
+        if (
+          user['portal_users'].find((e) => {
+            userId = e.id;
+            return e.email === email;
+          })
+        ) {
+          try {
+            const res = await this.httpService.post(this.NewIdeaUrl, {
+              idea: {
+                name: 'Bug Report',
+                description: message,
+                custom_fields: {
+                  category, // module
+                  needed_by, // needed_by
+                  type, // type
+                },
+                categories: type,
+                created_by_portal_user: {
+                  id: userId,
+                  name,
+                },
+              },
+            });
+
+            res.subscribe((x) => x.data);
+            // console.log(res.subscribe(x => x.data));
+            return { statusCode: 200, message: 'Added Bug Report' };
+          } catch (err) {
+            throw new BadRequestException(`Something Went Wrong!`);
+          }
+
+          // console.log('Post idea function', user);
+        }
+      });
+    };
+
+    // Function to find/create user
+    const create_user = async () => {
+      const get_portal_users = await this.getAllPortalUsers();
+
+      // checking and creating users.
+      get_portal_users.subscribe(async (x) => {
+        // console.log('PORTAL_USERS :', x.portal_users);
+        if (
+          !x['portal_users'].find((e) => {
+            userId = e.id;
+            return e.email === email;
+          })
+        ) {
+          try {
+            // creating portal user.
+            const result = await this.httpService.post(this.NewPortalUserUrl, {
+              portal_user: {
+                email: email,
+                first_name: first_name,
+                last_name: last_name,
+              },
+            });
+            result
+              .pipe(map((x) => x.data))
+              .subscribe((x) => console.log('result', x));
+          } catch (err) {
+            throw new BadRequestException(`Something Went Wrong!`);
+          }
+        }
+      });
+    };
+
+    // call post idea after we found the user or created the user
+    create_user();
+    setTimeout(() => {
+      post_idea();
+    }, 10000);
+
+    // create_user().then(post_idea);
+
+    return { statusCode: 200, message: 'User Added' };
+  }
+
+  async getAllPortalUsers(): Promise<any> {
+    // console.log('GetAllPortalUsers');
+
+    const resp = this.httpService
+      .get(this.NewPortalUserUrl)
+      .pipe(map((res) => res.data));
+
+    // resp.subscribe(x => console.log(x));
+
+    return resp;
   }
 
   async addFeedback(feedbackInfoDto: FeedbackInfoDto, instituteId: string) {
@@ -178,5 +284,113 @@ export class UtilityService {
       message: Responses.GUIDES_SUCCESS,
       data: guides,
     };
+  }
+
+  public sendReportEmail(
+    email: string[],
+    name: string,
+    userEmail: string,
+    message: string,
+    // screenshotsValue: string,
+    screenShotList: Array<string>,
+    type?: string,
+  ): void {
+    //function to send simple message
+
+    const date = new Date();
+    const offset = -300; //Timezone offset for EST in minutes.
+    const estDate = new Date(date.getTime() + offset * 60 * 1000);
+
+    if (screenShotList.length == 3) {
+      this.mailerService
+        .sendMail({
+          to: `${email}`, // List of receivers email address
+          from: 'pratik2018id@gmail.com', // Senders email address
+          subject: 'Bug Report from palette', // Subject line
+          text: ``, // plaintext body
+          html: `
+        <p>This is a Report Issue submitted on Palette app at ${estDate}.</p><br>
+        <p>Name: ${name}</p><br> <p>Email: ${userEmail}</p><br><p>Message: ${message}</p>
+        <br>
+        <a href="${screenShotList[0]}">screenshot1  
+        </a>
+        <br>
+        <a href="${screenShotList[1]}">screenshot2  
+        </a>
+        <br>
+        <a href="${screenShotList[2]}">screenshot3  
+        </a>    
+        `, // HTML body content
+        })
+        .then((success) => {
+          console.log(success);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (screenShotList.length == 2) {
+      this.mailerService
+        .sendMail({
+          to: `${email}`, // List of receivers email address
+          from: 'pratik2018id@gmail.com', // Senders email address
+          subject: 'Bug Report from palette', // Subject line
+          text: ``, // plaintext body
+          html: `
+      <p>This is a Report Issue submitted on Palette app at ${estDate}.</p><br>
+      <p>Name: ${name}</p><br> <p>Email: ${userEmail}</p><br><p>Message: ${message}</p>
+      <br>
+      <a href="${screenShotList[0]}">screenshot1  
+      </a>
+      <br>
+      <a href="${screenShotList[1]}">screenshot2  
+      </a>    
+      `, // HTML body content
+        })
+        .then((success) => {
+          console.log(success);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (screenShotList.length == 1) {
+      this.mailerService
+        .sendMail({
+          to: `${email}`, // List of receivers email address
+          from: 'pratik2018id@gmail.com', // Senders email address
+          subject: 'Bug Report from palette', // Subject line
+          text: ``, // plaintext body
+          html: `
+      <p>This is a Report Issue submitted on Palette app at ${estDate}.</p><br>
+      <p>Name: ${name}</p><br> <p>Email: ${userEmail}</p><br><p>Message: ${message}</p>
+      <br>
+      <a href="${screenShotList[0]}">screenshot1  
+      </a>   
+      `, // HTML body content
+        })
+        .then((success) => {
+          console.log(success);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      this.mailerService
+        .sendMail({
+          to: `${email}`, // List of receivers email address
+          from: 'pratik2018id@gmail.com', // Senders email address
+          subject: 'Bug Report from palette', // Subject line
+          text: ``, // plaintext body
+          html: `
+      <p>This is a Report Issue submitted on Palette app at ${estDate}.</p><br>
+      <p>Name: ${name}</p><br> <p>Email: ${userEmail}</p><br><p>Message: ${message}</p> 
+      `, // HTML body content
+        })
+        .then((success) => {
+          console.log(success);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
 }
